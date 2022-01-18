29eef65da7d6ae331ce909dc31323fb2d53d8387
Luis Marquez <englamc@gmail.com>
2022-01-18 14:10:49 -0400

    Pilas

--- First_Queues_Assignment/Source.cpp
+++ First_Queues_Assignment/Source.cpp
@@ -2,7 +2,8 @@
 
 //HEADERS
 #include <iostream>
-const int TAM = 50;
+const int TAM =  50;
+const int SIZE = 50;
 
 //DECLARACI[ON DE NOMBRES DE ESPACIO
 using namespace std;
@@ -132,6 +133,138 @@ class ColaCircular : public Cola <int> {
 		}
 };
 
+//Clase pila
+template <class dataType> class Stack {
+private:
+	//Arreglo que se convertirá en la pila
+	dataType stack[SIZE];
+
+	//Cima o tope, indica en que posición del arrego se encuentra el último dato ingresado
+	int top;
+
+public:
+	//Constructor
+	Stack();
+
+	//Retorna true si la pila está llena
+	bool isFull();
+
+	//Retorna true si la pila está vacía
+	bool isEmpty();
+
+	//Inserta un elemento en el tope de la pila
+	void push(dataType key);
+
+	//Extrae el elemento que se encuentre en el tope de la pila
+	void pop();
+
+	//Limpia toda la pila dejándola vacía
+	void clean();
+
+	//Obtiene el elemento en el tope de la pila y lo imprime sin extraerlo
+	void topStack();
+
+	//Obtiene el número de elemtos que tiene la pila
+	void length();
+
+	//Imprime toda la pila sin extraer los elementos
+	void print();
+};
+
+//Constructor
+template <class dataType> Stack<dataType>::Stack() {
+	top = -1;
+
+	for (int i = 0; i < SIZE; i++) {
+		stack[i] = NULL;
+	}
+}
+
+//Retorna true si la pila está llena
+template <class dataType> bool Stack<dataType>::isFull() {
+	if (top != SIZE - 1) {
+		return false;
+	}
+
+	return true;
+}
+
+//Retorna true si la pila está vacía
+template <class dataType> bool Stack<dataType>::isEmpty() {
+	if (top != -1) {
+		return false;
+	}
+
+	return true;
+}
+
+//Inserta un elemento en el tope de la pila
+template <class dataType> void Stack<dataType>::push(dataType key) {
+	if (this->isFull()) {
+		cerr << "ERROR: Error de OVERFLOW en la pila." << endl;
+		return;
+	}
+
+	top++;
+	stack[top] = key;
+	cout << "Elemento " << key << " insertado con exito." << endl;
+}
+
+//Extrae el elemento que se encuentre en el tope de la pila
+template <class dataType> void Stack<dataType>::pop() {
+	if (isEmpty()) {
+		cerr << "Error: Error de UNDERFLOW en la pila." << endl;
+		return;
+	}
+
+	dataType key;
+
+	key = stack[top];
+	top--;
+
+	cout << "Elemento " << key << " extraido con exito." << endl;
+}
+
+//Limpia toda la pila dejándola vacía
+template <class dataType> void Stack<dataType>::clean() {
+	if (this->isEmpty()) {
+		cerr << "Error: Error de UNDERFLOW en la pila." << endl;
+	}
+
+	//Pone tope en -1, de esta forma no se pueden acceder a los datos ya escritos y reescribiendo si quisieran guardar nuevos datos
+	top = -1;
+
+	cout << "La pila ha sido limpiada" << endl;
+}
+
+template <class dataType> void Stack<dataType>::topStack() {
+	if (this->isEmpty()) {
+		cerr << "Error: Error de UNDERFLOW en la pila." << endl;
+	}
+
+	cout << "El elemento en el tope de la pila es: " << stack[top] << endl;
+}
+
+template <class dataType> void Stack<dataType>::length() {
+	if (this->isEmpty()) {
+		cerr << "Error: Error la pila se encuentra vacía." << endl;
+	}
+
+	cout << "La pila tiene " << (top + 1) << " elementos." << endl;
+}
+
+//Imprime toda la pila sin extraer los elementos
+template <class dataType> void Stack<dataType>::print() {
+	if (isEmpty()) {
+		cerr << "Error: Error de UNDERFLOW en la pila." << endl;
+	}
+	for (int i = top; i >= 0; i--) {
+		if (stack[i] != NULL) {
+			cout << "Pos: " << i << ": " << stack[i] << endl;
+		}
+	}
+}
+
 //FUNCION PRINCIPAL
 int main(int args, const char* argsv[]) {
 	Cola <int> colita;
@@ -156,6 +289,15 @@ int main(int args, const char* argsv[]) {
 	cout << "El primer elemento es: " << colitaCircular.verPrimero() << endl;
 	colitaCircular.extraerElemento();
 	cout << "El primer elemento es: " << colitaCircular.verPrimero() << endl;*/
+
+	Stack <int> pila;
+	/*pila.push(10);
+	pila.push(20);
+	pila.push(30);
+	pila.push(40);
+	pila.topStack();
+	pila.pop();
+	pila.topStack();*/
 
 	return 0;
 }

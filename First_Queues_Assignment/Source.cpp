e698a566d8bbcfe77a424bc3f266531b6796845a
Luis Marquez <englamc@gmail.com>
2022-01-17 20:19:50 -0400

    Commit Inicial - Branch Dev

---
+++ First_Queues_Assignment/Source.cpp
@@ -0,0 +1,12 @@
+/**/
+
+//HEADERS
+#include <iostream>
+
+//DECLARACI[ON DE NOMBRES DE ESPACIO
+using namespace std;
+
+//FUNCION PRINCIPAL
+int main(int args, const char* argsv[]) {
+	return 0;
+}
